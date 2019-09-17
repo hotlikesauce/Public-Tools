7dd52d6b261f5cf5fd3b31deb37bed7c659c9d7f
bombtheblocks <46724986+bombtheblocks@users.noreply.github.com>
2019-09-17 08:57:32 -0600

    Update Remove Underscore.py

--- Remove Underscore.py
+++ Remove Underscore.py
@@ -3,4 +3,4 @@ root =("[YOUR PATH]")
 for path, folders, files in os.walk(root):
     print folders
     for f in files:
-        os.rename(os.path.join(path, f), os.path.join(path, f.replace('L_', '')))
+        os.rename(os.path.join(path, f), os.path.join(path, f.replace('_', '')))
