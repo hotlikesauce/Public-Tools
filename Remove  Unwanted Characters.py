9d23bd97e6d6c61b18f72f39c746174e7ee46399
bombtheblocks <46724986+bombtheblocks@users.noreply.github.com>
2019-09-24 09:24:41 -0600

    Update and rename Remove Underscore.py to Remove  Unwanted Characters.py

--- Remove  Unwanted Characters.py
+++ Remove  Unwanted Characters.py
@@ -1,3 +1,6 @@
+##This script will remove unwanted characters from a file name
+##I have it set up to remove underscores but change the underscore text in the last line to make it whatever you want
+
 import os
 root =("[YOUR PATH]")
 for path, folders, files in os.walk(root):
